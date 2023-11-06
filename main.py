cb88b835fb966ea9aacf70c2b02d44d5ced6d68b
James-the-1st <jamesgarang.162@gmail.com>
2023-11-06 13:47:57 +0300

    4 change in the code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ loan_amount = float(input("Enter the loan amount you're requesting: "))
 # Define the loan qualification criteria
 min_credit_score = 650
 min_annual_income = 25000
-max_loan_amount = 100000
+max_loan_amount = 100002
 
 # Check if the user qualifies for the loan
 if credit_score >= min_credit_score and annual_income >= min_annual_income and loan_amount <= max_loan_amount:
